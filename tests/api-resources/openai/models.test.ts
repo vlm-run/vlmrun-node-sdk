d4f46aab78c38e816a24db9886a4341c388d1a13
Shahrear <161046103+shahrear33@users.noreply.github.com>
2025-01-05 15:35:36 +0000

    feat: fix: updated class name

--- tests/api-resources/openai/models.test.ts
+++ tests/api-resources/openai/models.test.ts
@@ -1,9 +1,9 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-import Vlm from 'vlmrun';
+import VlmRun from 'vlmrun';
 import { Response } from 'node-fetch';
 
-const client = new Vlm({
+const client = new VlmRun({
   bearerToken: 'My Bearer Token',
   baseURL: process.env['TEST_API_BASE_URL'] ?? 'http://127.0.0.1:4010',
 });
@@ -24,7 +24,7 @@ describe('resource models', () => {
     // ensure the request options are being passed correctly by passing an invalid HTTP method in order to cause an error
     await expect(
       client.openai.models.retrieve('model', { path: '/_stainless_unknown_path' }),
-    ).rejects.toThrow(Vlm.NotFoundError);
+    ).rejects.toThrow(VlmRun.NotFoundError);
   });
 
   test('list', async () => {
@@ -41,7 +41,7 @@ describe('resource models', () => {
   test('list: request options instead of params are passed correctly', async () => {
     // ensure the request options are being passed correctly by passing an invalid HTTP method in order to cause an error
     await expect(client.openai.models.list({ path: '/_stainless_unknown_path' })).rejects.toThrow(
-      Vlm.NotFoundError,
+      VlmRun.NotFoundError,
     );
   });
 });
